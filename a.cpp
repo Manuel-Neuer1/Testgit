10820729711437897a3c159155e0533d30bc3d5a
Manuel-Neuer1 <2530254482@qq.com>
2023-11-08 11:56:21 +0800

    修改了 XX 文件，添加了 YY 文件

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cout << a << endl;
+    cout << "hello world!!!" << endl;
+    system("pause");
+
+    return 0;
+}
